02b0897388db43de8e60d84c1f44230bd789875b
Bagus <muhammadrizkibagus@gmail.com>
2023-11-24 08:28:52 +0700

    bagus

--- index.php
+++ index.php
@@ -25,8 +25,7 @@ $queryProduk = mysqli_query($con, "SELECT id, nama, harga, foto, detail FROM pro
             <div class="col-md-8 offset-md-2">
                 <form method="get" action="produk.php">
                     <div class="input-group input-group-lg my-4">
-                        <input type="text" class="form-control" placeholder="Cari Disini"
-                            aria-label="Recipient's username" aria-describedby="basic-addon2" name="keyword">
+                        <input type="text" class="form-control" placeholder="Cari Disini" aria-label="Recipient's username" aria-describedby="basic-addon2" name="keyword">
                         <button type="submit" class="btn warna1 text-white">Telusuri</button>
                     </div>
                 </form>
@@ -40,15 +39,13 @@ $queryProduk = mysqli_query($con, "SELECT id, nama, harga, foto, detail FROM pro
 
             <div class="row nt-5">
                 <div class="col-md-4 mb-3">
-                    <div
-                        class="highlighted-kategori kategori-baju-pria d-flex justify-content-center align-items-center">
+                    <div class="highlighted-kategori kategori-baju-pria d-flex justify-content-center align-items-center">
                         <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=Baju Pria">Baju
                                 Pria</a></h4>
                     </div>
                 </div>
                 <div class="col-md-4 mb-3">
-                    <div
-                        class="highlighted-kategori kategori-baju-wanita d-flex justify-content-center align-items-center">
+                    <div class="highlighted-kategori kategori-baju-wanita d-flex justify-content-center align-items-center">
                         <h4 class="text-white"><a class="no-decoration" href="produk.php?kategori=Baju Wanita">Baju
                                 Wanita</a></h4>
                     </div>
@@ -83,31 +80,30 @@ $queryProduk = mysqli_query($con, "SELECT id, nama, harga, foto, detail FROM pro
                 <h3>Produk</h3>
 
                 <div class="row mt-5">
-                    <?php while ($data = mysqli_fetch_array($queryProduk)) {
-                        ?>
+                    <?php while ($data = mysqli_fetch_array($queryProduk)) { ?>
                         <div class="col-sm-6 col-md-4 mb-3">
-                            <div class="card">
-                                <img src="image/<?php echo $data['foto'] ?>" class="card-img-top" alt="...">
+                            <div class="card h-100">
+                               <div class="image-box">
+                               <img src="image/<?php echo $data['foto']; ?>" class="card-img-top" alt="...">
+                               </div>
                                 <div class="card-body">
-                                    <h4 class="card-title">Card
-                                        <?php echo $data['nama'] ?>
-                                    </h4>
-                                    <p class="card-text text-truncate">
-                                        <?php echo $data['detail'] ?>
-                                    </p>
-                                    <p class="card-text text-harga">Rp
-                                        <?php echo $data['harga'] ?>
-                                    </p>
-                                    <a href="produk-detail.php?nama='$data[nama]'" class="btn warna2 text-white">Lihat
-                                        Detail</a>
+                                    <h4 class="card-title"><?php echo $data['nama']; ?></h4>
+                                    <p class="card-text text-truncate"><?php echo $data['detail']; ?></p>
+                                    <p class="card-text text-harga">Rp <?php echo $data['harga']; ?></p>
+                                    <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 ">Lihat Detail</a>
                                 </div>
                             </div>
                         </div>
                     <?php } ?>
                 </div>
+
+                <a class="btn btn-outline-warning mt-3 p-3 fs-3" href="produk.php">See More</a>
+
             </div>
         </div>
 
+        
+        <?php require "footer.php"; ?>
         <script src=" bootstrap/js/bootstrap.bundle.min.js">
         </script>
         <script src="fontawesome/js/all.min.js"></script>
