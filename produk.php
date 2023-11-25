50e51e8b211279dabe15e68f3d3389dcff3c9149
ditaismi218 <ismidita21@gmail.com>
2023-11-25 17:19:48 +0700

    memperbaiki produk.php, menambahkan halaman produk-detail.php dan tentang-kami.php

--- produk.php
+++ produk.php
@@ -3,18 +3,24 @@ require "koneksi.php";
 
 $queryKategori = mysqli_query($con, "SELECT * FROM kategori");
 
+// get produk by nama produk/keyword
 if (isset($_GET['keyword'])) {
     $queryProduk = mysqli_query($con, "SELECT * FROM produk WHERE nama LIKE '%$_GET[keyword]%'");
-} else if (isset($_GET['kategori'])) {
-    $queryGetKategoriId = mysqli_query($con, "SELECT id FROM kategori WHERE nama='$_GET[kategori]'");
-    $queryGetKategoriId = mysqli_fetch_array($queryGetKategoriId);
+}
+// get produk by kategori
+else if (isset($_GET['kategori'])) {
+    $queryGetKategoriId = mysqli_query($con, "SELECT * FROM kategori WHERE nama='$_GET[kategori]'");
+    $kategoriId = mysqli_fetch_array($queryGetKategoriId);
 
     $queryProduk = mysqli_query($con, "SELECT * FROM produk WHERE kategori_id='$kategoriId[id]'");
-} else {
+}
+// get produk default
+else {
     $queryProduk = mysqli_query($con, "SELECT * FROM produk");
 }
 
 $countData = mysqli_num_rows($queryProduk);
+
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +29,7 @@ $countData = mysqli_num_rows($queryProduk);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toko Online |Produk</title>
+    <title>Produk</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
@@ -32,113 +38,70 @@ $countData = mysqli_num_rows($queryProduk);
 <body>
     <?php require "navbar.php"; ?>
 
+    <!-- banner -->
     <div class="container-fluid banner-produk d-flex align-items-center">
         <div class="container">
-            <h1 class="text-white text-center">Produk</h1>
+            <h1 class="text-white text-center">produk</h1>
         </div>
     </div>
 
+    <!-- body -->
     <div class="container py-5">
         <div class="row">
-
             <div class="col-lg-3 mb-5">
                 <h3>Kategori</h3>
                 <ul class="list-group">
                     <?php while ($kategori = mysqli_fetch_array($queryKategori)) { ?>
-                        <a class="no-decoration" href="produk.php?kategori=<?php echo $kategori['nama']; ?>">
-                            <li class="list-group-item"><?php echo $kategori['nama']; ?></li>
+                        <a class="no-decoration" href="produk.php?kategori=<?php echo $kategori['nama'] ?>">
+                            <li class="list-group-item">
+                                <?php echo $kategori['nama']; ?>
+                            </li>
                         </a>
                     <?php } ?>
                 </ul>
             </div>
-        </div>
-        <div class="col-lg-9">
-            <h3 class="text-center mb-3">Produk</h3>
-            <div class="row"></div>
-            <?php
-            if ($countData < 1) {
-            ?>
-                <h4 class="text-center my-5">KALO NYARI BARANG YANG NGOTAK LAH BLOK</h4>
-            <?php
-            }
-            ?>
-
-
-            <?php while ($produk = mysqli_fetch_array($queryProduk)) { ?>
-                <div class="col-md-4 mb-4"></div>
-                <div class="card h-100">
-                    <div class="image-box">
-                        <img src="image/SOAL1_1.png" class="card-img-top" alt="...">
-                    </div>
-                    <div class="card-body">
-                        <h4 class="card-title">whatever</h4>
-                        <p class="card-text text-truncate">whatever</p>
-                        <p class="card-text text-harga">Rp 231321321</p>
-                        <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 ">Lihat Detail</a>
-                    </div>
+            <div class="col-lg-9">
+                <h3 class="text-center mb-3">Produk</h3>
+                <div class="row">
+                    <?php
+                    if ($countData == 0) {
+                        ?>
+                        <h4 class="text-center my-3">Produk yang Anda cari tidak tersedia</h4>
+                        <?php
+                    }
+                    ?>
+                    <?php while ($produk = mysqli_fetch_array($queryProduk)) { ?>
+                        <div class="col-md-4 mb-5">
+                            <div class="card h-100">
+                                <div class="image-box">
+                                    <img src="image/<?php echo $produk['foto']; ?>" class="card-img-top" alt="...">
+                                </div>
+                                <div class="card-body">
+                                    <h4 class="card-title">
+                                        <?php echo $produk['nama']; ?>
+                                    </h4>
+                                    <p class="card-text text-truncate">
+                                        <?php echo $produk['detail']; ?>
+                                    </p>
+                                    <p class="card-text text-harga">Rp
+                                        <?php echo $produk['harga']; ?>
+                                    </p>
+                                    <a href="produk-detail.php?nama=<?php echo $produk['nama']; ?>"
+                                        class="btn warna2 ">Lihat
+                                        Detail</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
+            </div>
         </div>
-    <?php } ?>
-    <div class="card h-100">
-        <div class="image-box">
-            <img src="image/SOAL1_1.png" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-            <h4 class="card-title">whatever</h4>
-            <p class="card-text text-truncate">whatever</p>
-            <p class="card-text text-harga">Rp 231321321</p>
-            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 ">Lihat Detail</a>
-        </div>
-    </div>
-    <div class="card h-100">
-        <div class="image-box">
-            <img src="image/SOAL1_1.png" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-            <h4 class="card-title">whatever</h4>
-            <p class="card-text text-truncate">whatever</p>
-            <p class="card-text text-harga">Rp 231321321</p>
-            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 ">Lihat Detail</a>
-        </div>
-    </div>
-    <div class="card h-100">
-        <div class="image-box">
-            <img src="image/SOAL1_1.png" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-            <h4 class="card-title">whatever</h4>
-            <p class="card-text text-truncate">whatever</p>
-            <p class="card-text text-harga">Rp 231321321</p>
-            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 ">Lihat Detail</a>
-        </div>
-    </div>
-    <div class="card h-100">
-        <div class="image-box">
-            <img src="image/SOAL1_1.png" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-            <h4 class="card-title">whatever</h4>
-            <p class="card-text text-truncate">whatever</p>
-            <p class="card-text text-harga">Rp 231321321</p>
-            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 ">Lihat Detail</a>
-        </div>
-    </div>
-    <div class="card h-100">
-        <div class="image-box">
-            <img src="image/SOAL1_1.png" class="card-img-top" alt="...">
-        </div>
-        <div class="card-body">
-            <h4 class="card-title">whatever</h4>
-            <p class="card-text text-truncate">whatever</p>
-            <p class="card-text text-harga">Rp 231321321</p>
-            <a href="produk-detail.php?nama=<?php echo $data['nama']; ?>" class="btn warna2 ">Lihat Detail</a>
-        </div>
-    </div>
-    </div>
     </div>
 
-    <script src=" bootstrap/js/bootstrap.bundle.min.js">
-    </script>
+    <!-- footer -->
+    <?php require "footer.php"; ?>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="fontawesome/js/all.min.js"></script>
 </body>
 
---
+++ tentang-kami.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Star Fashion | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php require "navbar.php"; ?>
+
+    <!-- banner -->
+    <div class="container-fluid banner-produk d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+
+    <!-- main -->
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam delectus molestias libero vero quam
+                temporibus enim cumque maxime voluptates dolore error at facere, accusamus asperiores illum culpa
+                debitis. Esse voluptatem quidem quo, ratione expedita obcaecati iusto minus culpa ad voluptas ex placeat
+                commodi sit exercitationem accusamus aperiam repellendus voluptate recusandae asperiores rem. Dolorum
+                sed aperiam, voluptates consequatur dignissimos hic assumenda nihil mollitia eveniet officia quas odio
+                minima. Odit numquam minima non pariatur similique distinctio voluptate, obcaecati a, impedit vel
+                quisquam neque expedita ipsa sequi nihil est velit quam officiis hic exercitationem harum. Itaque,
+                cupiditate eos. Eaque voluptates in nihil beatae?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore saepe aliquam dolorem suscipit sapiente
+                architecto mollitia ad eos assumenda ab ea, maiores quidem tempore? Nesciunt alias modi qui, ducimus
+                impedit veniam delectus excepturi a deserunt explicabo sit soluta. Commodi minima fugiat rem iusto quos
+                animi! Dignissimos facilis suscipit obcaecati possimus aspernatur ullam atque at repudiandae mollitia
+                ratione, doloremque consectetur laboriosam est dolorem quam illum magni illo voluptas non enim adipisci!
+                Saepe ipsum nostrum aut similique voluptatibus aperiam deleniti ipsa laboriosam?
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa sint, doloribus placeat nam dignissimos
+                inventore officiis nisi praesentium atque nemo mollitia accusamus fugit libero ducimus tempora itaque
+                labore. Non pariatur delectus similique ut, qui, recusandae fugit eaque, autem deserunt magnam illo
+                facilis! Vero error reprehenderit illum consequuntur. Vero, ex nisi. Officia, recusandae. Nobis ratione
+                velit numquam facere architecto aut autem exercitationem dolore tempore quo, ut odio ducimus aperiam
+                excepturi temporibus magni illo? Alias provident vitae obcaecati! Rerum optio vitae illo inventore porro
+                cupiditate, placeat corporis soluta iste, id culpa consequatur voluptatem molestias dicta alias! Neque
+                veritatis, fugiat nulla odio, incidunt in facere voluptatibus, officiis ipsa sapiente nihil accusamus
+                pariatur optio! Eum cum quia dicta temporibus quisquam repellendus aliquam rerum deserunt minima id,
+                voluptas hic repellat eius et optio, nemo alias eveniet atque saepe tenetur, quibusdam doloribus qui
+                magni. Animi aut hic saepe quam a ducimus libero quasi iusto repellendus exercitationem!
+            </p>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php require "footer.php"; ?>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+
+</html>
